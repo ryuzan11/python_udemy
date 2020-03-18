4a0bbb3027ddb774bfd55447dc7e86e65b7d8529
逢坂 <osaka@truss.company>
2020-03-18 13:56:53 +0900

    edit

--- lesson.py
+++ lesson.py
@@ -1,3 +1,10 @@
-from termcolor import colored
+class Person(object):
+    def __init__(self, name, hobby = 'car'):
+        self.name = name
+        self.hobby = hobby
 
-print(colored('test', 'red'))
+    def say_something(self):
+        print('I am {}. hello'.format(self.name))
+
+person = Person(name = 'Mike')
+person.say_something()
